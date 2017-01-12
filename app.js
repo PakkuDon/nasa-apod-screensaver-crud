311287af11e598b75b4cd8ef52967faf67317e65
PakkuDon <pakku.pd@gmail.com>
2017-01-12 12:36:00 +1100

    Fetch APOD images and save to DB

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
-var app = express();
 var fetch = require('isomorphic-fetch');
+var moment = require('moment');
+var app = express();
 var db = require('./db');
 const API_KEY = require('./apod_key');
 
@@ -18,6 +19,36 @@ app.get('/api/pictures', (req, res) => {
   db.query('select * from pictures')
     .then(data => {
       res.status(200).json(data);
+    });
+});
+
+// GET /api/pictures/load
+// Fetch images from APOD API and store in database
+app.get('/api/pictures/load', (req, res) => {
+  // Generate dates from last few weeks
+  var dates = [];
+  var date = moment();
+  date.subtract(1, 'days');
+  for (var i = 0; i < 10; i++) {
+    dates.push(date.format('YYYY-MM-DD'));
+    date.subtract(1, 'weeks');
+  }
+
+  // Get pictures for these days
+  var urls = dates.map(date => `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`);
+  Promise.all(urls.map(url => {
+    return fetch(url)
+      .then(res => res.json());
+    }))
+    .then(images => {
+      Promise.all(images.map(image => {
+        return db.none('INSERT INTO pictures(title, explanation, url) VALUES($1, $2, $3)', [
+          image.title, image.explanation, image.hdurl
+        ]);
+      }))
+      .then(() => {
+        res.status(200).send('Images fetched successfully');
+      })
     });
 });
 
